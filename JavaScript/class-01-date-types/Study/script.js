2cc5312ada81a305a05ea76e5c2e6c7c53d55547
Hilderlandio Alcantara <hilderlandio@gmail.com>
2022-11-07 15:59:39 -0300

    l

--- JavaScript/class-01-date-types/Study/script.js
+++ JavaScript/class-01-date-types/Study/script.js
@@ -1,53 +1,9 @@
-// primitive Values
-// (Strings , NUmber, Boolean)
-//
+let name = prompt(`digite seu nome  :`)
+alert(`olá ${name}  !!! Ozório digita algo a sguir  .`)
 
-//let name = "JC Dedex"
-//console.log(name)
-//console.log(typeof name)
+let firstNumber = Number(prompt(`digite um number`))
+let secundNumber = Number(prompt(`digite outro number `))
 
-//let age = 29 
-//funções Construção tipos variável
-//age = String(age)
-//console.log(age)
-//console.log(typeof age)
+soma = firstNumber + secundNumber
 
-//let num = 1;
-//let num29 = 5
-//let num3 = "2"
-
-//let numFloat = 1.0 ; //float
-//console.log(num, numFloat)
-
-//variable scope(hosting) = elevar
-
-{
-  //  var num5 = 2
-    //console.log(num5)
-    {
-        let num5 =3
-        console.log(num5)
-    }
-  // console.log(num5)
-}
-//variaveis constaNTES ex: const pi = 3.14
-/*
-let num1 = 10
-let num2 = 20
-let total = num1 + num2
-
-console.log(`a soma de ${num1}e ${num2} é igual a ${total}` )*/
-
-
- /*==================*/
- let name= "fulano"
- let idade= 30
- let prof= "prog"
-
- pessoa : {
-    name; "fulano",
-    idade; 30,
-    prof; Prog 
- }
-
-
+alert(`o valor eh ${soma}`)
